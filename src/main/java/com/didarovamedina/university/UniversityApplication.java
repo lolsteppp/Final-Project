78e251b36444c368bbe94d3c2ce25fe7393f3c5a
medina <medidid0509@gmail.com>
2026-05-12 16:30:59 +0500

    add changes in main

--- src/main/java/com/didarovamedina/university/UniversityApplication.java
+++ src/main/java/com/didarovamedina/university/UniversityApplication.java
@@ -2,12 +2,27 @@ package com.didarovamedina.university;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class UniversityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UniversityApplication.class, args);
     }
 
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
 }
